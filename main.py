6d466e60fd7dc2bace279e7dfbb1d3a9da1fb0c8
Mardirooster <samuelmardirosian@gmail.com>
2022-07-19 13:24:56 -0400

    Add function to create json per image to parallel loop

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import time
 
 img_folder = "panoptic_val2017/"
 json_folder = "image_json/" # folder for per-image json outputs
-short_file = "short_file.json"
+short_file = "short_file.json" # an edited version of panoptic_val2017.json for testing
 full_file = "panoptic_val2017.json"
 
 #img_info is from the "images" list in the original json
-def create_img_json(img_info):
+def create_img_json(img_info,annotations,categories):
     img_name = img_info["file_name"]
     imp_name_png = img_name.split(".")[0] + ".png"
     img_location = img_folder + imp_name_png
@@ -26,6 +26,48 @@ def create_img_json(img_info):
         "height": img_info["height"],
         "segments_info": []
     }
+
+    img = cv2.imread(img_location)
+    segments_json = next(item for item in annotations if item["file_name"] == imp_name_png)
+    for segment in segments_json["segments_info"]:
+        #print(segment)
+        #hex_id = format(segment["id"],"x")
+        # convert the id into RGB
+        # per the COCO website, ids=R+G*256+B*256^2,
+        # so the ids can be read as hex RGB color codes
+        rgb_id = list(ImageColor.getcolor("#"+'{:06X}'.format(segment["id"]),"RGB"))
+        mask = np.all(img==rgb_id, axis=2)
+        height,width = mask.shape
+
+        flat_mask = mask.flatten()
+
+        # get points where mask[x] =! mask[x+1]
+        # [0,0,0,1,1,0] -> [False, False,  True, False,  True]
+        diff_mask = flat_mask[1:] != flat_mask[:-1]
+        # get positions of these points
+        # [False, False,  True, False,  True] -> [2,4]
+        positions = np.where(diff_mask)
+        # starting from -1, use np diff to get the lengths of each segment by
+        # subtracting each position
+        # first, expanding [2,4] to [-1,2,4,5]
+        # [-1,2,4,5] -> [3,2,1]
+        counts = np.diff(np.append(-1,positions,len(flat_mask)-1))
+
+
+        category = next(item for item in categories if item["id"] == segment["category_id"])
+
+        segment_info = {
+            "id": segment["id"],
+            "category_name": category["name"],
+            "mask": {
+                "counts": counts.tolist(),
+                "width": width,
+                "height": height
+            }
+        }
+
+        img_dict["segments_info"].append(segment_info)
+        #print(counts)
 
     img_json = json.dumps(img_dict, indent=4)
 
@@ -45,7 +87,7 @@ def main():
 #    for image in data["images"]:
 #        print(json.dumps(image, indent = 4, sort_keys=True))
     start = time.time()
-    Parallel(n_jobs=4)(delayed(create_img_json)(img_info) for img_info in data["images"])  
+    Parallel(n_jobs=12)(delayed(create_img_json)(img_info,data["annotations"],data["categories"]) for img_info in data["images"])  
     stop = time.time()
     duration = stop - start
     print("Parallel:" + str(duration))
@@ -59,68 +101,6 @@ def main():
 
     return
 
-    img_json = data["images"][1]
-    img_png = img_json["file_name"].split(".")[0] + ".png"
-    img_location = img_folder + img_png
-#    img = np.array(Image.open(img_location))    
-
-    img = cv2.imread(img_location)
-
-
-    all_rgb_codes = img.reshape(-1, img.shape[-1])
-
-    unique_rgbs = np.unique(all_rgb_codes, axis=0)
-
-    print("Unique:")
-    print(str(unique_rgbs))
-
-    # cv2.imshow("image!", img, img_bgr)
-
-    # cv2.waitKey(0) 
-    # cv2.destroyAllWindows() 
-
-    # get the segments entry where the file_name is the img_png
-    segments_json = next(item for item in data["annotations"] if item["file_name"] == img_png)
-    print(segments_json)
-    for segment in segments_json["segments_info"]:
-        rle = []
-        print(segment)
-        #hex_id = format(segment["id"],"x")
-        rgb_id = list(ImageColor.getcolor("#"+format(segment["id"],"x"),"RGB"))
-        print(rgb_id)
-        mask = np.all(img==rgb_id, axis=2)
-        print(mask)
-        height,width = mask.shape
-        print(height)
-        print()
-
-        flat_mask = mask.flatten()
-
-        # get points where mask[x] =! mask[x+1]
-        # [0,0,0,1,1,0] -> [False, False,  True, False,  True]
-        diff_mask = flat_mask[1:] != flat_mask[:-1]
-        # get positions of these points
-        # [False, False,  True, False,  True] -> [2,4]
-        positions = np.where(diff_mask)
-        # starting from -1, use np diff to get the lengths of each segment by
-        # subtracting each position
-        # first, expanding [2,4] to [-1,2,4,5]
-        # [-1,2,4,5] -> [3,2,1]
-        counts = np.diff(np.append(-1,positions,len(flat_mask)-1))
-        print(counts)
-
-
-
-    # coco notes that ids=R+G*256+B*256^2
-
-
-    # ids=R+G*256+B*256^2
-    #  [ 58 132 101]
-    #  [ 90 128 144]]
-
-    # to rgb: [ 101 132 58  ]
-
-    # 58 + (132*256) + (101 * 256 *256)
 
 
 
